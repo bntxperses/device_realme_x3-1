534b4e5919be8f31da474787d18ff64ecbbe030e
Perses <dbikramyt@gmail.com>
2022-11-17 17:36:57 +0000

    x3:Revert: Move BT profiles props to vendor
    Signed-off-by: Perses <dbikramyt@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -147,7 +147,7 @@ TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
-TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/system_ext.prop
 
 # Recovery
 BOARD_INCLUDE_RECOVERY_DTBO := true
